1cdd590af694c54a13d035704b4952ef8c6726ae
Federico De Leonardis <federico.deleonardis@studio.unibo.it>
2025-03-23 10:35:44 +0100

    Campio sasso

--- TemperatureMonitoring/src/include/set_up.h
+++ TemperatureMonitoring/src/include/set_up.h
@@ -11,7 +11,7 @@
 #define WIFI_SSID "Home&Life SuperWiFi-6985"
 #define WIFI_PASSWORD "Tamburini78"
 #define MQTT_SERVER "broker.mqtt-dashboard.com" //
-#define TOPIC_PER "IOT-Progetto-03-period"
+#define TOPIC_PER "IOT-Progetto-03-per"
 #define TOPIC_TEMP "IOT-Progetto-03-temp"
 
 #endif
